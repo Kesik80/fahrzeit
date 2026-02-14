3b0e9f79e11ca9164ee6f471609d6105ace8f52e
Kesik80 <55840881+Kesik80@users.noreply.github.com>
2026-02-15 00:50:40 +0100

    Upload 3 files via web interface

--- stations.js
+++ stations.js
@@ -1,23 +1,22 @@
 // реестр имен
 const defaultCoords = {
-  'Sergii': {lat:51.530133, lng:6.850858},
-  'Anatolii': {lat:51.522694, lng:6.858944},
-  'Andrii': {lat:51.534558, lng:6.861061},
-  'Sergio': {lat:51.52, lng:6.887619},
-  'Dima': {lat:51.551753, lng:7.102285},
-  'René': {lat:51.476281, lng:6.861856},
-  'OLGA-Park': {lat:51.5004335, lng:6.8686337},
-  'Marvin': {lat:51.556472, lng:6.731424},
-  'Vasyl': {lat:51.515355, lng:6.893655}
+  'Sergii':   {lat: 51.530133,  lng: 6.850858},
+  'Anatolii': {lat: 51.522694,  lng: 6.858944},
+  'Andrii':   {lat: 51.534558,  lng: 6.861061},
+  'Sergio':   {lat: 51.52,      lng: 6.887619},
+  'Dima':     {lat: 51.551753,  lng: 7.102285},
+  'René':     {lat: 51.476281,  lng: 6.861856},
+  'Marvin':   {lat: 51.556472,  lng: 6.731424},
+  'Vasyl':    {lat: 51.515355,  lng: 6.893655}
 };
 
 // реестр станций
 this.stations = {
-  'Gelsenkirchen-Bismarck': {lat:51.538627, lng:7.108037},
-  'Hagen': {lat:51.373517, lng:7.461144},
-  'Essen West': {lat:51.4539273, lng:6.9794622},
-  'Gelsenkirchen Horst': {lat:51.5415351, lng:7.0202811},
-  'Gelsenkirchen Horst 2': {lat:51.539072, lng:7.0090119}
+  'Gelsenkirchen-Bismarck':  {lat: 51.538627,  lng: 7.108037},
+  'Hagen':                   {lat: 51.373517,  lng: 7.461144},
+  'Essen West':              {lat: 51.4539273, lng: 6.9794622},
+  'Gelsenkirchen Horst':     {lat: 51.5415351, lng: 7.0202811},
+  'Gelsenkirchen Horst 2':   {lat: 51.539072,  lng: 7.0090119}
 };
 
 // Время в пути между участниками
@@ -45,7 +44,7 @@ this.travelTimes = {
   'Maikel-Marvin': 5,
   'René-Sergii': 15,
   'René-Andrii': 15,
-  'René-Anatolii': 15,
+  'René-Anatolii': 10,
   'René-Sergio': 15,
   'René-Dima': 25,
   'René-Marvin': 25,
@@ -57,7 +56,17 @@ this.travelTimes = {
   'Vasyl-Dima': 20,
   'Vasyl-Marvin': 25,
   'Vasyl-Maikel': 25,
-  'Vasyl-René': 25,
+  'Vasyl-René': 25
+};
+
+// Пункты встречи (Treffpunkt)
+this.treffpunkt = {
+  'OLGA-Park':                {lat: 51.5004335, lng: 6.8686337},
+  'Gelsenkirchen-Treffpunkt': {lat: 51.530304,  lng: 7.110901}
+};
+
+// Время в пути от пункта встречи до каждого участника
+this.trefftravelTimes = {
   'OLGA-Park-Sergii': 10,
   'OLGA-Park-Andrii': 10,
   'OLGA-Park-Anatolii': 10,
@@ -66,14 +75,23 @@ this.travelTimes = {
   'OLGA-Park-Marvin': 20,
   'OLGA-Park-Maikel': 20,
   'OLGA-Park-René': 5,
-  'OLGA-Park-Vasyl': 10
+  'OLGA-Park-Vasyl': 10,
+  'Gelsenkirchen-Treffpunkt-Sergii': 30,
+  'Gelsenkirchen-Treffpunkt-Andrii': 30,
+  'Gelsenkirchen-Treffpunkt-Anatolii': 30,
+  'Gelsenkirchen-Treffpunkt-Sergio': 25,
+  'Gelsenkirchen-Treffpunkt-Dima': 10,
+  'Gelsenkirchen-Treffpunkt-Marvin': 20,
+  'Gelsenkirchen-Treffpunkt-Maikel': 20,
+  'Gelsenkirchen-Treffpunkt-René': 35,
+  'Gelsenkirchen-Treffpunkt-Vasyl': 25
 };
 
 // Приоритетные списки остановок
 this.participantOrders = {
-  'Sergii': ['Anatolii', 'Andrii', 'Sergio', 'Dima'],
-  'Anatolii': ['Sergii', 'Andrii', 'Sergio', 'Dima', 'Marvin'],
-  'Andrii': ['Sergii', 'Anatolii', 'Sergio', 'Dima'],
-  'Sergio': ['Anatolii', 'Sergii', 'Andrii', 'Dima'],
-  'Dima': ['Anatolii', 'Sergii', 'Andrii', 'Sergio']
+  'Sergii':   ['Anatolii', 'Sergio', 'Vasyl'],
+  'Anatolii': ['Sergii', 'Andrii', 'Sergio', 'Vasyl'],
+  'Andrii':   ['Sergii', 'Anatolii', 'Sergio', 'Dima'],
+  'Sergio':   ['Anatolii', 'Sergii', 'Andrii', 'Dima'],
+  'Dima':     ['Anatolii', 'Sergii', 'Andrii', 'Sergio']
 };
