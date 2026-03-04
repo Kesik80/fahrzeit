83e9643c5cdd8ce8219c150bc5a561f657727bd5
Kesik80 <55840881+Kesik80@users.noreply.github.com>
2026-03-04 03:11:54 +0100

    web-update stations.js

--- stations.js
+++ stations.js
@@ -137,7 +137,5 @@ const defaultGreetingsData = [
 const defaultMachineDestinationsData = [
   { name: 'an der Maschine', lat: null, lng: null },
   { name: 'im Büro Bochum', lat: 51.503883, lng: 7.240038 },
-  { name: 'an der Sauna', lat: 51.5093, lng: 6.903112 },
-  { name: 'Новая', lat: 51.5093, lng: 6.903112 },
-  { name: 'Новая точка', lat: 51.5093, lng: 6.903112 }
+  { name: 'an der Sauna', lat: 51.5093, lng: 6.903112 }
 ];
