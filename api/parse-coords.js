04040736d5adcae82d9aa2f965ae965cfb126d31
Kesik80 <55840881+Kesik80@users.noreply.github.com>
2026-03-06 14:28:53 +0100

    Upload 1 file(s)

--- api/parse-coords.js
+++ api/parse-coords.js
@@ -136,6 +136,9 @@ function parseCoordsFromBody(body) {
 
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') { res.status(200).end(); return; }
   res.setHeader('Content-Type', 'application/json');
 
   const input = (
