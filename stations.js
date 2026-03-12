e056910c70ec19e290cda58f0d538fb063fb48f1
Kesik80 <55840881+Kesik80@users.noreply.github.com>
2026-03-12 05:18:50 +0100

    web-update stations.js

--- stations.js
+++ stations.js
@@ -139,5 +139,5 @@ const defaultMachineDestinationsData = [
   { name: 'im Büro Bochum', lat: 51.503883, lng: 7.240038 },
   { name: 'an der Sauna', lat: 51.5093, lng: 6.903112 },
   { name: 'Hotel "Am Stadtpark"', lat: 51.1783189, lng: 9.3683781 },
-  { name: 'Opladen Plasser', lat: 51.0591072, lng: 7.012385 }
+  { name: 'Opladen Plasser', lat: 51.0593175, lng: 7.012283399999999 }
 ];
