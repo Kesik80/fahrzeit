fdbc3ba35de098094365de7c4fc92f856eee269b
Kesik80 <55840881+Kesik80@users.noreply.github.com>
2026-06-23 01:20:41 +0200

    web-editor 2026-06-22 23:20

--- stations.js
+++ stations.js
@@ -17,7 +17,6 @@ this.stations = {
   'Gelsenkirchen-Bismarck': {lat: 51.538627, lng: 7.108037},
   'Hagen': {lat: 51.373517, lng: 7.461144},
   'Essen West': {lat: 51.4539273, lng: 6.9794622},
-  'Gelsenkirchen Horst 2': {lat: 51.539072, lng: 7.0090119},
   'Dortmund-Lütgendortmund': {lat: 51.497711, lng: 7.3665302},
   'Köln Kalk Nord': {lat: 50.9400354, lng: 7.0158018},
   'Gutersloh': {lat: 51.907774, lng: 8.3882563},
@@ -34,7 +33,8 @@ this.stations = {
   'Gevelsberg-West': {lat: 51.316939, lng: 7.314008},
   'Wuppertal-Langerfeld': {lat: 51.278872, lng: 7.244835},
   'Recklinghausen': {lat: 51.568298, lng: 7.198117},
-  'Gelsenkirchen Horst Nord': {lat: 51.5415351, lng: 7.0202811}
+  'Gelsenkirchen Horst Nord': {lat: 51.5415351, lng: 7.0202811},
+  'Gelsenkirchen Horst Nord 2': {lat: 51.539072, lng: 7.0090119}
 };
 
 // Время в пути между участниками
